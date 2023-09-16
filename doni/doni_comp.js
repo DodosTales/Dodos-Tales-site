dc19aaa2202571d680020c7b96d9adcc3a29db48
alessandrobasi <13107787+alessandrobasi@users.noreply.github.com>
2023-09-16 17:23:38 +0200

    doni ok & fix

--- doni/doni_comp.js
+++ doni/doni_comp.js
@@ -1,10 +1,10 @@
 "use strict";
 
-import { doni_comp } from "../main.js";
+import { doni_comp } from "/assets/js/main.js";
 
 const donicomp = document.querySelector(".donicomp");
 
-for (let i=0; i<doni_comp.length; i++){
+for (let i = 0; i < doni_comp.length; i++) {
     let newLi = document.createElement("li");
     newLi.classList.add("coltext");
     newLi.textContent = doni_comp[i];
