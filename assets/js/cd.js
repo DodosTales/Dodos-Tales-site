0d20d1c52e074b5a3c9e75204bb8a8c70c017f1f
alessandrobasi <13107787+alessandrobasi@users.noreply.github.com>
2023-09-16 17:09:06 +0200

    reazione,karma e cd ok

--- assets/js/cd.js
+++ assets/js/cd.js
@@ -1,25 +1,29 @@
 "use strict";
 
-import { battute } from "../main.js";
+import { battute } from "/assets/js/main.js";
 
-window.onload = ()=>{
+window.onload = () => {
     const calcolo = document.getElementById("calcolo");
     const result = document.getElementById("result");
     const drey = document.getElementById("drey");
     const spazio = document.getElementById("spazio");
 
-    calcolo.addEventListener('click', () =>{
+    calcolo.addEventListener("click", () => {
         const livello = Number(document.getElementById("livello").value);
         const primaria = Number(document.getElementById("primaria").value);
         const mana = Number(document.getElementById("mana").value);
 
-        let cd = (livello-livello%4)/4 + primaria + (mana-mana%5)/5 + 11;
+        let cd =
+            (livello - (livello % 4)) / 4 +
+            primaria +
+            (mana - (mana % 5)) / 5 +
+            11;
         result.classList.add("border");
-        result.innerText=String(cd);
+        result.innerText = String(cd);
     });
 
-    drey.addEventListener('click', ()=>{
-        let ran = Math.floor(Math.random() * (battute.length));
-        spazio.innerText=battute[ran];
+    drey.addEventListener("click", () => {
+        let ran = Math.floor(Math.random() * battute.length);
+        spazio.innerText = battute[ran];
     });
-}
+};
