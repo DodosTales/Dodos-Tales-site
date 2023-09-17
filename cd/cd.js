930b6718e1a0dc2f2e1cdafbb78d60e7d1831c71
Giorgio Dn <giorgiodn05@gmail.com>
2023-09-17 11:41:56 +0200

    Aggiunto un commento per prova

--- cd/cd.js
+++ cd/cd.js
@@ -13,6 +13,8 @@ window.onload = ()=>{
         const primaria = Number(document.getElementById("primaria").value);
         const mana = Number(document.getElementById("mana").value);
 
+        //approssimo il livello ed il mana per ottenere interi dalla divisione 
+        //la formula per la CD era 10 + 1 ogni 4 livelli (partendo da 1) + 1 ogni 5 mana spesi + caratteristica
         let cd = (livello-livello%4)/4 + primaria + (mana-mana%5)/5 + 11;
         result.classList.add("border");
         result.innerText=String(cd);
