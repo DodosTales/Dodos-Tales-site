b1b46f539d7699c83e1b321bbc49a1230c1ff05e
Giorgio Dn <giorgiodn05@gmail.com>
2024-01-05 12:44:02 +0100

    Adding description of CD, add the past timelines and the date of the present timeline

--- assets/js/timeline_storia.js
+++ assets/js/timeline_storia.js
@@ -1,12 +1,514 @@
 const DATA = [
-    ["", "[...]", ""],
-    ["Tempo Presente", null],
+    ["La Creazione", null],
+
     [
-        "Il caso ha voluto...",
-        "La nostra avventura inizia oggi, un evento segnerà l'inizio della campagna e dell'avventura di 7 avventurieri, riuniti sotto il nome dei Lyons' Pride",
-        "979 EM",
-        "/assets/img/dodo.png",
+        "L'inizio di ogni cosa", 
+        "Inizialmente non vi era nulla, se non una singola entità, era onnipotente ma neppure lei poteva sfuggire al più grande nemico di tutti… la noia, dunque prese una decisione, e si divise in tre creando ciò che ora le razze mortali chiamano Karma, tre entità dai caratteri estremamente diversi eppure estremamente simili fra loro. <br> Per non essere più soli le tre entità crearono il mondo che oggi conosciamo come Eltharia, Helyos, la stella che illumina e riscalda il pianeta ed il firmamento, rischiarato da due Lune Selenia e Lunaria. <br> Dopodichè decisero che il mondo sarebbe stato sorretto da quattro pilastri, ponti per altre realtà. Per finire si divisero il compito di popolare il mondo con ogni tipo di forma di vita; <br> Il Karma nero, amante della forza e del caos, diede vita agli animali e piante ed alle razze mortali, ciascuna con le sue caratteristiche peculiari così che il mondo non sarebbe mai stato noioso. <br> Il Karma bianco, rigoroso e ed intransigente, un numeroso Phanteon di divinità ed i draghi, creature dal potere sconfinato, che avevano il compito di vegliare su Eltharia e su le creature che lo abitavano. <br> Per finire, il Karma grigio donò ad ogni cosa la magia, così che anche la più debole delle creature sarebbe potuta divenire potentissima con la giusta dose di fortuna ed impegno. <br> Conclusa la sua opera, il Karma scomparve, limitandosi ad osservare l’operato delle loro creazioni, seppur di quando in quando, offrendo il loro supporto sotto forma di biglie capaci di manipolare la realtà a coloro che reputano meritevoli.", 
+        "???", 
+        null
     ],
+
+    ["Pandora la storia del continente", null],
+
+    [
+        "", 
+        "È difficile poter definire una data d’inizio, quando Eltharia nacque le razze ora conosciute erano ancora primitive e i primi Draghi al fianco degli Dei, si occupavano di vegliare sui mortali senza mai intromettersi seguendo fedelmente le direttive del Karma bianco. <br>Con il passare dei secoli però, con la scomparsa del Karma e l'evolversi delle razze mortali, le Divinità persero interesse nelle razze mortali ed abbandonarono Eltharia, creando nuovi piani e nuove creature su cui dominare, interessandosi di rado del loro antico compito.<br>Le nuove generazioni dei draghi, meno potenti dei loro predecessori, trovatisi mancare progressivamente l’assistenza degli dei, per paura di non poter più adempiere il loro compito con la giusta efficienza decisero che non era più tempo di vegliare come sui mortali senza però intervenire direttamente.<br>Benchè questa decisione non fosse condivisa da tutti i Draghi, questi sottomisero tutte le razze mortali del mondo allora conosciuto, fondando quello che oggi è conosciuto come 'L’impero dei draghi'.<br>Questo impero durò per millenni, e si estendeva su tutto il continente, era diviso in grandi regioni, ciascuna gestita da un gruppo di Draghi. Fatta eccezione per alcune rare eccezioni, che godevano di un particolare rispetto da parte dei Draghi e dunque una maggior libertà, le altre Razze sotto il loro giogo erano schiavizzate, ed oppresse. <br>Una piccola parte delle regioni dell’impero, erano gestite da una fazione di draghi più benevola, contraria alla condotta violenta e dispotica dei loro fratelli e prediligeva una convivenza pacifica con i mortali. <br>Nel corso della lunga vita dell’impero furono molteplici i tentativi da parte dei mortali di liberarsi dal giogo dell’impero, ma l’eccezionale abilità nell’uso della magia e la loro quasi totale immunità ad essa rendeva quasi impossibile per i mortali di poter uccidere un Drago. ", 
+        "???", 
+        null
+    ],
+    
+    [
+        "Durante l'impero dei draghi...", 
+        "Una piccola regione all’estremo sud di Eltharia, si ribella a Galakron, signore di quella regione, il risultato sembrava scontato, ma, grazie all’uso di una magia mai vista prima, il Drago venne sconfitto ed ucciso. <br>Si pensa che il primo uso della magia dei 'Dragon slayer' risalga a questo periodo, ma quale sia il primo Drago che abbia insegnato ai mortali tale magia rimane ancora nel mistero ", 
+        "-227 ID", 
+        null
+    ],
+
+    [
+        "L'inizio della 'guerra della 1⁰ Piaga'", 
+        "Una feroce ritorsione da parte degli altri draghi soffocò rapidamente le varie rivolte nella regione, ma quando raggiunsero la città dove Galakron era stato ucciso, gli abitanti, in preda alla disperazione, utilizzarono una magia oscura e corrotta, liberando sul mondo un male ben peggiore di quello che volevano contrastare.<br> La città venne immediatamente inghiottita da un enorme portale che condusse su Eltharia un esercito infinito di creature estranee al Karma stesso. <br> Questi esseri, spietati, intelligenti e ben organizzati, utilizzavano un tipo di magia sconosciuto in grado di ferire ed uccidere indistintamente Draghi e mortali con grande facilità; erano inoltre in grado di corrompere tutto ciò su cui passavano, trasformando il terreno in una landa desolata inadatta alla vita. ", 
+        "-226 ID", 
+        null
+    ],
+
+    [
+        "La fine della 'guerra della 1⁰ Piaga'", 
+        "La guerra si protrae per più di 100 anni ed i Draghi, un tempo ritenuti invincibili subiscono innumerevoli vittime, ma, battaglia dopo battaglia, riescono a ricacciare i Demoni oltre il portale, concludendo così la guerra.", 
+        "-94 ID", 
+        null
+    ],
+
+    [
+        "Rivolte", 
+        "I Draghi seppur vincitori uscirono dalla guerra estremamente indeboliti e si trovarono ben presto costretti a fronteggiare una nuova minaccia, di fatto, in tutto l’impero, i mortali, approfittando della distrazione dei loro signori, si erano organizzati ed ora nuove rivolte sorgevano in ogni angolo dell’impero", 
+        "-93 ID", 
+        null
+    ],
+
+    [
+        "", 
+        "I Draghi che fino a quel momento avevano gestito le regioni in modo pacifico, permettendo ai mortali di prosperare, si unirono alla lotta in loro favore, assieme a maghi in grado di utilizzare una magia in grado di ferire quegli esseri immortali, la magia del 'Dragon Slayer'. <br>Per la prima volta Maghi e Draghi combatterono con lo stesso obbiettivo, rovesciare quell’impero oppressivo che per secoli si era macchiato di innumerevoli crimini.", 
+        "-34 ID", 
+        null
+    ],
+
+    [
+        "L'inizio dell'era dei mortali", 
+        "La definitiva caduta dell'impero dei Draghi è considerata l’inizio dell’era moderna, l’era dei mortali. <br>Dopo la caduta dell’impero, il numero di draghi era enormemente ridotto, i pochi che si erano arresi o dati alla fuga la maggior parte faceva parte della fazione di draghi benevoli. <br>Dopo tanto sangue versato, i draghi rimasti si ritirarono in isolamento o raggiunsero i piani creati dagli Dei, solo in pochi rimasero a contatto con i mortali. ", 
+        "0 EM", 
+        null
+    ],
+
+    [
+        "", 
+        "Passarono dei secoli, e quello che era il territorio del vecchio impero andò a frammentarsi in innumerevoli piccoli regni dei mortali, i quali, senza più un nemico comune iniziarono a farsi la guerra fra di loro per i più disparati motivi. ", 
+        "431 EM", 
+        null
+    ],
+
+    [
+        "L'inizio della 'guerra della 2⁰ Piaga'", 
+        "Senza alcun preavviso dal luogo in cui fu aperta la breccia secoli prima, apparvero nuovamente i demoni, come un’inondazione dilagarono per Eltharia massacrando e corrompendo ogni forma di vita sul loro cammino. Durante la prima guerra si trovarono a fronteggiare un saldo impero comandato da potenti entità, ma ora i regni erano divisi e deboli, questo concesse loro di avanzare per miglia sul territorio quasi incontrastati. <br>I regni dei mortali si trovano costretti a coalizzarsi per fronteggiare una minaccia che potrebbe portare alla fine di Eltharia stessa, viene fondato il 'Grande consiglio della magia' un organo extra governativo in grado di operare su tutti i territori delle nazioni. Grazie ad esso, le nazioni riuscirono a coalizzarsi ed unire i loro eserciti contro la minaccia incombente, e grazie al supporto dei pochi draghi rimasti riuscirono ad arrestare l’avanzata demoniaca. ", 
+        "467 EM", 
+        null
+    ],
+
+    [
+        "Il grande cataclisma", 
+        "Grazie alla coalizione delle nazioni ed al Grande Consiglio i mortali iniziarono ad ottenere diverse vittorie contro gli eserciti dei Demoni, ma la vittoria sembrava ancora lontana. <br>Ma, un lord dei demoni di nome Baal, riuscì a farsi largo con il suo esercito fino al Pilastro dell’Aria e dopo aver sterminato i Therian che lo proteggevano, lo corruppe, trasformando ogni forma di vita per miglia e miglia in qualcosa di oscuro e malevolo. (Le creature senzienti che riuscirono a mantenere coscienza di sé divennero la racca che oggi è conosciuta come Tiefling) <br>Con la distruzione del pilastro elementale, ne mortali ne draghi erano più in grado di utilizzare la magia in quell’area che giorno dopo giorno sembrava espandersi sempre di più. ", 
+        "481 EM", 
+        null
+    ],
+
+    [
+        "La discesa degli dei", 
+        "La distruzione del pilastro mostrò agli dei, qualcosa che non avevano considerato, benchè si fossero allontanati e disinteressati da Eltharia, erano ancora legati ad essa, di fatti il Karma aveva fatto in modo che il destino del mondo fosse legato al loro. Il cataclisma li aveva enormemente indeboliti e scoperta questa tremenda realtà si ripromisero che mai più avrebbero ignorato il loro antico compito. <br>Quando ormai la guerra sembrava perduta, schiere di creature celestiali, create dagli Dei giunsero in soccorso dei mortali, spostando l’ago della bilancia in loro favore.", 
+        "490 EM", 
+        null
+    ],
+
+    [
+        "Il grande bastione", 
+        "Dopo una sanguinosa guerra, le orde demoniache vennero respinte oltre la zona corrotta, ma ormai il Pilastro era perduto, la zona invivibile e praticamente impossibile da strappare alle mani dei Demoni. Perciò, venne innalzato un immenso muro, il quale, grazie alla magia angelica, era in grado di arginare l’incedere della corruzione. <br>Esistevano ancora sacche di demoni in giro per il mondo, ma troppo piccole per risultare una minaccia. ", 
+        "534 EM", 
+        null
+    ],
+
+    [
+        "La fine della 'guerra della 2⁰ Piaga'", 
+        "Ben presto i Demoni smisero di attaccare il muro e si ritirarono nelle profondità del territorio che avevano corrotto.<br>Le sacche di demoni oltre il muro scomparvero senza lasciare tracce, si pensa si siano mescolati fra i mortali aspettando il momento giusto per tornare nuovamente all’opera. <br>La maggior parte delle creature angeliche tornò sul loro piano di origine, ma i pochi che decisero di mescolarsi con gli umani, diedero origine alla stirpe degli Asimar. ", 
+        "539 EM", 
+        null
+    ],
+
+    [
+        "", 
+        "Le nazioni che sopravvissero al conflitto, si ripromisero che mai più sarebbero state divise, poiché la minaccia dei demoni permaneva, perciò il Grande consiglio della magia si fece garante di questa loro promessa.", 
+        "540 EM", 
+        null
+    ],
+
+    [
+        "", 
+        "Sono ormai passati secoli, regni sono caduti e sorti, fatta eccezione di qualche sporadico assalto fallimentare al Bastione, i demoni dono rimasti sopiti. <br>Senza più un nemico comune, in molti regni le promesse di alleanza sono venute meno, in più occasioni sono scoppiati conflitti e non sono poche le nazioni che hanno negato l’autorità del 'Gran consiglio'. <br>Benchè goda ancora di un notevole rispetto, il 'Gran consiglio della magia', non è mai stato così debole.", 
+        "975 EM", 
+        null
+    ],
+
+    [
+        "La scomparsa dei draghi", 
+        "I pochi draghi rimasti a contatto con i mortali sono svaniti nel nulla da un giorno all’altro. <br>Questo ha scatenato forti tensioni fra le nazioni, poiché la presenza di un drago nel proprio regno era considerata una formidabile difesa ed un deterrente per gli altri paesi. ", 
+        "977 EM", 
+        null
+    ],
+
+    ["Tempo Presente, Anno: 979 EM", null],
+
+    [
+        "",
+        "",
+        "12 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "13 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "14 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "15 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "16 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "17 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "18 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "20 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "21 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "22 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "23 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "24 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "25 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "26 seconda semina",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "26 brillaluce",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "28 brillaluce",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "29 brillaluce",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "30 brillaluce",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "4 luce del cielo",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "5 luce del cielo",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "9 luce del cielo",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "10 luce del cielo",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "14 luce del cielo",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "15 luce del cielo",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "17 luce del cielo",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "21 luce del cielo",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "22 luce del cielo",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "27 luce del cielo",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "28 luce del cielo",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "29 luce del cielo",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "30 luce del cielo",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "7 ultima messe",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "8 ultima messe",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "12 ultima messe",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "14 ultima messe",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "17 ultima messe",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "18 ultima messe",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "19 ultima messe",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "25 ultima messe",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "27 ultima messe",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "28 ultima messe",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "29 ultima messe",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "30 ultima messe",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "31 ultima messe",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "5 focolare",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "7 focolare",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "11 focolare",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "12 focolare",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "14 focolare",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "18 focolare",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "19 focolare",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "20 focolare",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "21 focolare",
+        null,
+    ],
+
+    [
+        "",
+        "",
+        "26 focolare",
+        null,
+    ],
+    
+    [
+        "",
+        "",
+        "27 focolare",
+        null,
+    ],
+
 ];
 
 const template = (titolo, corpo, data, img) => {
