0bf0c790e57948183f5895b9fc9273d1129a64ba
Giorgio Dn <giorgiodn05@gmail.com>
2024-03-28 21:07:48 +0100

    Fixed merging bug in cd.js

--- assets/js/cd.js
+++ assets/js/cd.js
@@ -12,18 +12,14 @@ window.onload = () => {
         const livello = Number(document.getElementById("livello").value);
         const primaria = Number(document.getElementById("primaria").value);
         const mana = Number(document.getElementById("mana").value);
-
-<<<<<<< HEAD:cd/cd.js
         //approssimo il livello ed il mana per ottenere interi dalla divisione 
         //la formula per la CD era 10 + 1 ogni 4 livelli (partendo da 1) + 1 ogni 5 mana spesi + caratteristica
-        let cd = (livello-livello%4)/4 + primaria + (mana-mana%5)/5 + 11;
-=======
+        //let cd = (livello-livello%4)/4 + primaria + (mana-mana%5)/5 + 11;
         let cd =
             (livello - (livello % 4)) / 4 +
             primaria +
             (mana - (mana % 5)) / 5 +
             11;
->>>>>>> ale:assets/js/cd.js
         result.classList.add("border");
         result.innerText = String(cd);
     });
