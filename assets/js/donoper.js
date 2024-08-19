ea60fb259c4ffa2d489f14a28f2fadc0de7b76bd
Giorgio Dn <giorgiodn05@gmail.com>
2024-08-19 20:26:23 +0200

    Rimosso il dono classico

--- assets/js/donoper.js
+++ assets/js/donoper.js
@@ -1,17 +1,11 @@
 "use strict";
 
 import { changeSVG, DiceSVG } from "/Dodos-Tales-site/assets/js/dice_svg.js";
-import { doni_comp, doni_mag } from "/Dodos-Tales-site/assets/js/main.js";
+import { doni_comp} from "/Dodos-Tales-site/assets/js/main.js";
 
-const maggiori = document.getElementById("magg");
 const comp = document.getElementById("comp");
-const exmaggiori = document.getElementById("exmagg");
-const extramagg = document.getElementById("extramag");
 const excomp = document.getElementById("excomp");
 const extracomp = document.getElementById("extracomp");
-const magdiv = document.getElementById("magdiv");
-const listamag = document.querySelectorAll(".listamag");
-const compdiv = document.getElementById("compdiv");
 const listacomp = document.querySelectorAll(".listacomp");
 
 function getRandomInt(max) {
@@ -32,10 +26,6 @@ function singoloDono(elem, array) {
                 <div class='col coltext'>${array[ran]}</div>
             </div>`;
 }
-
-maggiori.addEventListener("click", () => multipleDoni(listamag, doni_mag));
-
-exmaggiori.addEventListener("click", () => singoloDono(extramagg, doni_mag));
 
 comp.addEventListener("click", () => multipleDoni(listacomp, doni_comp));
 
